89ffb57e18fbaa1d3f4206422c893b2b8f739153
kw456 <46826017+kw456@users.noreply.github.com>
2022-02-17 13:18:51 -0500

    Added comments to vga-solver.c

--- Lab1/vga-solver.c
+++ Lab1/vga-solver.c
@@ -17,7 +17,6 @@
 #include <sys/time.h>
 #include <math.h>
 #include <pthread.h>
-//#include "address_map_arm_brl4.h"
 
 // graphics primitives
 void VGA_text(int, int, char *);
@@ -25,10 +24,7 @@ void VGA_text_clear();
 void VGA_box(int, int, int, int, short);
 void VGA_rect(int, int, int, int, short);
 void VGA_line(int, int, int, int, short);
-void VGA_Vline(int, int, int, short);
-void VGA_Hline(int, int, int, short);
-void VGA_disc(int, int, int, short);
-void VGA_circle(int, int, int, int);
+
 // 16-bit primary colors
 #define red (0 + (0 << 5) + (31 << 11))
 #define dark_red (0 + (0 << 5) + (15 << 11))
@@ -67,7 +63,7 @@ int colors[] = {red, dark_red, green, dark_green, blue, dark_blue,
 //#define HW_REGS_SPAN        0x00200000
 #define HW_REGS_SPAN 0x00005000
 
-// the light weight buss base
+// the light weight bus base
 void *h2p_lw_virtual_base;
 // HPS_to_FPGA FIFO status address = 0
 volatile unsigned int *lw_pio_ptr = NULL;
@@ -83,8 +79,7 @@ volatile signed int *lw_pio_beta_ptr = NULL;
 volatile signed int *lw_pio_rho_ptr = NULL;
 volatile signed int *lw_pio_dt_ptr = NULL;
 
-// read offset is 0x10 for both busses
-// remember that eaxh axi master bus needs unique address
+// Eaxh PIO port has its own address offset
 #define FPGA_PIO_LW_WRITE 0x00
 #define FPGA_PIO_LW_READ 0x10
 #define FPGA_PIO_READ_x 0x20
@@ -101,7 +96,6 @@ volatile signed int *lw_pio_dt_ptr = NULL;
 // pixel buffer
 volatile unsigned int *vga_pixel_ptr = NULL;
 void *vga_pixel_virtual_base;
-
 // character buffer
 volatile unsigned int *vga_char_ptr = NULL;
 void *vga_char_virtual_base;
@@ -109,6 +103,7 @@ void *vga_char_virtual_base;
 // /dev/mem file id
 int fd;
 
+// used for graphing curves using VGA_line()
 signed int x_loc;
 signed int y_loc;
 signed int z_loc;
@@ -119,26 +114,16 @@ signed int prev_z_loc;
 #define TRUE 1
 #define FALSE 0
 
+// input buffer for keyboard input
 char input_buffer[64];
-// access to enter condition
-// -- for signalling enter done
-pthread_mutex_t display_lock = PTHREAD_MUTEX_INITIALIZER;
-// access to print condition
-// -- for signalling print done
-pthread_mutex_t input_lock = PTHREAD_MUTEX_INITIALIZER;
-// the two condition variables related to the mutex above
-pthread_cond_t display_cond;
-pthread_cond_t input_cond;
 // control signals
 volatile int restart_flag = 0;
 volatile int reset_flag = 0;
 volatile int time_interval = 3000;
 volatile int pause_signal = 0;
 
-/* create a message to be displayed on the VGA
-        and LCD displays */
+// Printing labels and parameter values 
 char text_top_row[40] = "DE1-SoC ARM/FPGA\0";
-// char text_bottom_row[40] = "Cornell ece5760\0";
 char text_next[40] = "Lab1 ODE Solver\0";
 char sigma[40] = "sigma: 10.0";
 char beta[40] = "beta: 2.667";
@@ -146,9 +131,10 @@ char rho[40] = "rho: 28.0";
 char xz[40] = "XZ Projection";
 char yz[40] = "YZ Projection";
 char xy[40] = "XY Projection";
+// Used to iterate through colors array
 char color_index = 0;
-int color_counter = 0;
 
+// Converts float to fixed point number
 int to_fixed(float f, int e)
 {
   double a = f * pow(2, e);
@@ -163,31 +149,10 @@ int to_fixed(float f, int e)
   return b;
 }
 
+/******** Display Thread *******/
 void *display()
 {
-  // reset the kernel
-  // *(lw_pio_ptr) = 3;
-  // *(lw_pio_ptr) = 2;
-  // *(lw_pio_ptr) = 1;
-  // *(lw_pio_ptr) = 0;
-
-  // *(lw_pio_ptr) = 1;
-  // *(lw_pio_ptr) = 0;
-  // *(lw_pio_ptr) = 1;
-  // *(lw_pio_ptr) = 0;
-
-  // signed int x_loc = *(lw_pio_read_x_ptr);
-  // signed int y_loc = *(lw_pio_read_y_ptr);
-  // signed int z_loc = *(lw_pio_read_z_ptr);
-  // signed int prev_x_loc, prev_y_loc, prev_z_loc;
-
-  // *(lw_pio_ptr) = 1;
-  // *(lw_pio_ptr) = 0;
-  // prev_x_loc = x_loc;
-  // prev_y_loc = y_loc;
-  // prev_z_loc = z_loc;
-
-  // ---------------
+  // Convert floats to fixed point (7.20)  
   *lw_pio_init_x_ptr = to_fixed(-1.0, 20);
   *lw_pio_init_y_ptr = to_fixed(0.1, 20);
   *lw_pio_init_z_ptr = to_fixed(25.0, 20);
@@ -196,11 +161,13 @@ void *display()
   *lw_pio_rho_ptr = to_fixed(28.0, 20);
   *lw_pio_dt_ptr = to_fixed(1. / 256, 20);
 
+  // Reset kernel
   *(lw_pio_ptr) = 3;
   *(lw_pio_ptr) = 2;
   *(lw_pio_ptr) = 1;
   *(lw_pio_ptr) = 0;
 
+  // Update locations of current point and previous point (for graphing)
   x_loc = *(lw_pio_read_x_ptr);
   y_loc = *(lw_pio_read_y_ptr);
   z_loc = *(lw_pio_read_z_ptr);
@@ -208,28 +175,19 @@ void *display()
   prev_y_loc = y_loc;
   prev_z_loc = z_loc;
 
-  // printf("display thread before cv\n");
-  // sleep(1);
-  // pthread_cond_signal(&input_cond);
-
   while (1)
   {
-
-    // // wait for input done
-    // pthread_mutex_lock(&display_lock);
-    // printf("here1\n");
-    // pthread_cond_wait(&display_cond, &display_lock);
-    // printf("here2\n");
-
+    // If reset
     while (reset_flag == 1)
     {
-      // reset the kernel
+      // clear screen and reset the kernel
       VGA_box(0, 0, 639, 479, 0x0000);
       color_index = 10;
       *(lw_pio_ptr) = 3;
       *(lw_pio_ptr) = 2;
       *(lw_pio_ptr) = 1;
       *(lw_pio_ptr) = 0;
+      // If restart, clear screen and update the point locations
       if (restart_flag == 1)
       {
         VGA_box(0, 0, 639, 479, 0x0000);
@@ -244,7 +202,7 @@ void *display()
         break;
       }
     }
-
+    // If not reset or paused
     if ((reset_flag == 0) & (pause_signal == 0))
     {
       *(lw_pio_ptr) = 1;
@@ -253,29 +211,27 @@ void *display()
       y_loc = *(lw_pio_read_y_ptr);
       z_loc = *(lw_pio_read_z_ptr);
 
+      // Go back to beginning of color array if at the end
       if (color_index++ == 11)
         color_index = 0;
 
+      // Graph line between previous and current point, resize/scale within screen 
       VGA_line(160 + (int)(x_loc / 150000000.0 * 640), 100 + (int)(z_loc / 150000000.0 * 480), 160 + (int)(prev_x_loc / 150000000.0 * 640),
                100 + (int)(prev_z_loc / 150000000.0 * 480), colors[color_index]);
       VGA_line(480 + (int)(x_loc / 150000000.0 * 640), 150 + (int)(y_loc / 150000000.0 * 480), 480 + (int)(prev_x_loc / 150000000.0 * 640),
                150 + (int)(prev_y_loc / 150000000.0 * 480), colors[color_index]);
       VGA_line(320 + (int)(y_loc / 150000000.0 * 640), 275 + (int)(z_loc / 150000000.0 * 480), 320 + (int)(prev_y_loc / 150000000.0 * 640),
                275 + (int)(prev_z_loc / 150000000.0 * 480), colors[color_index]);
+      // Update previous location with current location
       prev_x_loc = x_loc;
       prev_y_loc = y_loc;
       prev_z_loc = z_loc;
-      // printf("display thread before mutex unlock\n");
-      // // unlock the input_buffer
-      // pthread_mutex_unlock(&display_lock);
-      // printf("display thread after mutex unlock\n");
-      // // and tell read1 thread that print is done
-      // pthread_cond_signal(&input_cond);
-      // printf("display thread after cv\n");
-
+      
+      // Delay (controls how fast we're sending clock signal to FPGA)
       usleep(time_interval);
     }
 
+    // If paused, stay in while loop until unpaused 
     while (pause_signal == 1)
     {
       if (pause_signal == 0)
@@ -284,34 +240,36 @@ void *display()
   }
 }
 
+/******** Input Thread *******/
 void *input()
 {
   while (1)
   {
-    // // wait for print done
-    // pthread_mutex_lock(&input_lock);
-    // printf("here3\n");
-    // pthread_cond_wait(&input_cond, &input_lock);
-    // printf("here4\n");
-    // the actual enter
+    // Display command and received value
     printf("Display Command: ");
     scanf("%s", input_buffer);
     printf("received value: %s\n", input_buffer);
 
+    // "s" = slow (add 500 us to clock cycle time)
     if (strcmp(input_buffer, "s") == 0)
       time_interval = time_interval + 500;
+    // "f" = slow (subtract 500 us to clock cycle time)
     else if (strcmp(input_buffer, "f") == 0)
       time_interval = time_interval - 500;
+    // "p" = pause
     else if (strcmp(input_buffer, "p") == 0)
       pause_signal = 1;
+    // "p" = unpause / resume
     else if (strcmp(input_buffer, "u") == 0)
       pause_signal = 0;
+    // "p" = reset
     else if (strcmp(input_buffer, "r") == 0)
     {
       reset_flag = 1;
       restart_flag = 0;
       printf("Default Condition (y/n): ");
       scanf("%s", input_buffer);
+      // Set to default initial values/parameters
       if (strcmp(input_buffer, "y") == 0)
       {
         *lw_pio_init_x_ptr = to_fixed(-1.0, 20);
@@ -323,6 +281,7 @@ void *input()
         *lw_pio_dt_ptr = to_fixed(1. / 256, 20);
         restart_flag = 1;
       }
+      // User inputs custom values for init positions, parameters, & dt
       else
       {
         printf("initial x position: ");
@@ -350,8 +309,8 @@ void *input()
         scanf("%s", input_buffer);
         *lw_pio_dt_ptr = to_fixed(strtof(input_buffer, NULL), 20);
 
+        // Rewrite text with updated parameters
         VGA_text_clear();
-        // write text
         VGA_text(10, 1, text_top_row);
         VGA_text(10, 2, text_next);
         VGA_text(10, 3, sigma);
@@ -363,19 +322,11 @@ void *input()
         restart_flag = 1;
       }
     }
-    // printf("input thread before mutex unlock\n");
-    // // unlock the input_buffer
-    // pthread_mutex_unlock(&input_lock);
-    // printf("input thread after mutex unlock\n");
-    // // and tell write1 thread that enter is complete
-    // pthread_cond_signal(&display_cond);
-    // printf("input thread after cv\n");
   }
 }
 
 int main(void)
 {
-
   // === need to mmap: =======================
   // FPGA_CHAR_BASE
   // FPGA_ONCHIP_BASE
@@ -389,7 +340,7 @@ int main(void)
     return (1);
   }
 
-  // get virtual addr that maps to physical
+  // Get virtual addr that maps to physical
   h2p_lw_virtual_base = mmap(NULL, HW_REGS_SPAN, (PROT_READ | PROT_WRITE), MAP_SHARED, fd, HW_REGS_BASE);
   if (h2p_lw_virtual_base == MAP_FAILED)
   {
@@ -397,7 +348,7 @@ int main(void)
     close(fd);
     return (1);
   }
-  // Get the addresses that map to the two parallel ports on the light-weight bus
+  // Get the addresses that map to all the parallel ports on the light-weight bus
   lw_pio_ptr = (unsigned int *)(h2p_lw_virtual_base + FPGA_PIO_LW_WRITE);
   lw_pio_read_ptr = (unsigned int *)(h2p_lw_virtual_base + FPGA_PIO_LW_READ);
   lw_pio_read_x_ptr = (signed int *)(h2p_lw_virtual_base + FPGA_PIO_READ_x);
@@ -420,10 +371,8 @@ int main(void)
     close(fd);
     return (1);
   }
-
   // Get the address that maps to the FPGA LED control
   vga_char_ptr = (unsigned int *)(vga_char_virtual_base);
-
   // === get VGA pixel addr ====================
   // get virtual addr that maps to physical
   vga_pixel_virtual_base = mmap(NULL, SDRAM_SPAN, (PROT_READ | PROT_WRITE), MAP_SHARED, fd, SDRAM_BASE);
@@ -433,17 +382,15 @@ int main(void)
     close(fd);
     return (1);
   }
-
   // Get the address that maps to the FPGA pixel buffer
   vga_pixel_ptr = (unsigned int *)(vga_pixel_virtual_base);
 
-  //  clear the screen
+  // Clear the screen
   VGA_box(0, 0, 639, 479, 0x0000);
-  // clear the text
+  // Clear the text
   VGA_text_clear();
-  // write text
+  // Write text for title, parameters, and graph labels
   VGA_text(10, 1, text_top_row);
-  // VGA_text(10, 2, text_bottom_row);
   VGA_text(10, 2, text_next);
   VGA_text(10, 3, sigma);
   VGA_text(10, 4, beta);
@@ -452,13 +399,8 @@ int main(void)
   VGA_text(50, 33, xy);
   VGA_text(30, 55, yz);
 
-  int status;
   // the thread identifiers
   pthread_t thread_display, thread_input;
-
-  // the condition variables
-  pthread_cond_init(&display_cond, NULL);
-  pthread_cond_init(&input_cond, NULL);
 
   // For portability, explicitly create threads in a joinable state
   //  thread attribute used here to allow JOIN
@@ -552,10 +494,6 @@ void VGA_box(int x1, int y1, int x2, int y2, short pixel_color)
   for (row = y1; row <= y2; row++)
     for (col = x1; col <= x2; ++col)
     {
-      // 640x480
-      // pixel_ptr = (char *)vga_pixel_ptr + (row<<10)    + col ;
-      //  set pixel color
-      //*(char *)pixel_ptr = pixel_color;
       VGA_PIXEL(col, row, pixel_color);
     }
 }
@@ -601,10 +539,6 @@ void VGA_rect(int x1, int y1, int x2, int y2, short pixel_color)
   col = x1;
   for (row = y1; row <= y2; row++)
   {
-    // 640x480
-    // pixel_ptr = (char *)vga_pixel_ptr + (row<<10)    + col ;
-    //  set pixel color
-    //*(char *)pixel_ptr = pixel_color;
     VGA_PIXEL(col, row, pixel_color);
   }
 
@@ -612,10 +546,6 @@ void VGA_rect(int x1, int y1, int x2, int y2, short pixel_color)
   col = x2;
   for (row = y1; row <= y2; row++)
   {
-    // 640x480
-    // pixel_ptr = (char *)vga_pixel_ptr + (row<<10)    + col ;
-    //  set pixel color
-    //*(char *)pixel_ptr = pixel_color;
     VGA_PIXEL(col, row, pixel_color);
   }
 
@@ -623,10 +553,6 @@ void VGA_rect(int x1, int y1, int x2, int y2, short pixel_color)
   row = y1;
   for (col = x1; col <= x2; ++col)
   {
-    // 640x480
-    // pixel_ptr = (char *)vga_pixel_ptr + (row<<10)    + col ;
-    //  set pixel color
-    //*(char *)pixel_ptr = pixel_color;
     VGA_PIXEL(col, row, pixel_color);
   }
 
@@ -634,219 +560,6 @@ void VGA_rect(int x1, int y1, int x2, int y2, short pixel_color)
   row = y2;
   for (col = x1; col <= x2; ++col)
   {
-    // 640x480
-    // pixel_ptr = (char *)vga_pixel_ptr + (row<<10)    + col ;
-    //  set pixel color
-    //*(char *)pixel_ptr = pixel_color;
-    VGA_PIXEL(col, row, pixel_color);
-  }
-}
-
-/****************************************************************************************
- * Draw a horixontal line on the VGA monitor
- ****************************************************************************************/
-#define SWAP(X, Y) \
-  do               \
-  {                \
-    int temp = X;  \
-    X = Y;         \
-    Y = temp;      \
-  } while (0)
-
-void VGA_Hline(int x1, int y1, int x2, short pixel_color)
-{
-  char *pixel_ptr;
-  int row, col;
-
-  /* check and fix box coordinates to be valid */
-  if (x1 > 639)
-    x1 = 639;
-  if (y1 > 479)
-    y1 = 479;
-  if (x2 > 639)
-    x2 = 639;
-  if (x1 < 0)
-    x1 = 0;
-  if (y1 < 0)
-    y1 = 0;
-  if (x2 < 0)
-    x2 = 0;
-  if (x1 > x2)
-    SWAP(x1, x2);
-  // line
-  row = y1;
-  for (col = x1; col <= x2; ++col)
-  {
-    // 640x480
-    // pixel_ptr = (char *)vga_pixel_ptr + (row<<10)    + col ;
-    //  set pixel color
-    //*(char *)pixel_ptr = pixel_color;
-    VGA_PIXEL(col, row, pixel_color);
-  }
-}
-
-/****************************************************************************************
- * Draw a vertical line on the VGA monitor
- ****************************************************************************************/
-#define SWAP(X, Y) \
-  do               \
-  {                \
-    int temp = X;  \
-    X = Y;         \
-    Y = temp;      \
-  } while (0)
-
-void VGA_Vline(int x1, int y1, int y2, short pixel_color)
-{
-  char *pixel_ptr;
-  int row, col;
-
-  /* check and fix box coordinates to be valid */
-  if (x1 > 639)
-    x1 = 639;
-  if (y1 > 479)
-    y1 = 479;
-  if (y2 > 479)
-    y2 = 479;
-  if (x1 < 0)
-    x1 = 0;
-  if (y1 < 0)
-    y1 = 0;
-  if (y2 < 0)
-    y2 = 0;
-  if (y1 > y2)
-    SWAP(y1, y2);
-  // line
-  col = x1;
-  for (row = y1; row <= y2; row++)
-  {
-    // 640x480
-    // pixel_ptr = (char *)vga_pixel_ptr + (row<<10)    + col ;
-    //  set pixel color
-    //*(char *)pixel_ptr = pixel_color;
-    VGA_PIXEL(col, row, pixel_color);
-  }
-}
-
-/****************************************************************************************
- * Draw a filled circle on the VGA monitor
- ****************************************************************************************/
-
-void VGA_disc(int x, int y, int r, short pixel_color)
-{
-  char *pixel_ptr;
-  int row, col, rsqr, xc, yc;
-
-  rsqr = r * r;
-
-  for (yc = -r; yc <= r; yc++)
-    for (xc = -r; xc <= r; xc++)
-    {
-      col = xc;
-      row = yc;
-      // add the r to make the edge smoother
-      if (col * col + row * row <= rsqr + r)
-      {
-        col += x; // add the center point
-        row += y; // add the center point
-        // check for valid 640x480
-        if (col > 639)
-          col = 639;
-        if (row > 479)
-          row = 479;
-        if (col < 0)
-          col = 0;
-        if (row < 0)
-          row = 0;
-        // pixel_ptr = (char *)vga_pixel_ptr + (row<<10) + col ;
-        //  set pixel color
-        //*(char *)pixel_ptr = pixel_color;
-        VGA_PIXEL(col, row, pixel_color);
-      }
-    }
-}
-
-/****************************************************************************************
- * Draw a  circle on the VGA monitor
- ****************************************************************************************/
-
-void VGA_circle(int x, int y, int r, int pixel_color)
-{
-  char *pixel_ptr;
-  int row, col, rsqr, xc, yc;
-  int col1, row1;
-  rsqr = r * r;
-
-  for (yc = -r; yc <= r; yc++)
-  {
-    // row = yc;
-    col1 = (int)sqrt((float)(rsqr + r - yc * yc));
-    // right edge
-    col = col1 + x; // add the center point
-    row = yc + y;   // add the center point
-    // check for valid 640x480
-    if (col > 639)
-      col = 639;
-    if (row > 479)
-      row = 479;
-    if (col < 0)
-      col = 0;
-    if (row < 0)
-      row = 0;
-    // pixel_ptr = (char *)vga_pixel_ptr + (row<<10) + col ;
-    //  set pixel color
-    //*(char *)pixel_ptr = pixel_color;
-    VGA_PIXEL(col, row, pixel_color);
-    // left edge
-    col = -col1 + x; // add the center point
-    // check for valid 640x480
-    if (col > 639)
-      col = 639;
-    if (row > 479)
-      row = 479;
-    if (col < 0)
-      col = 0;
-    if (row < 0)
-      row = 0;
-    // pixel_ptr = (char *)vga_pixel_ptr + (row<<10) + col ;
-    //  set pixel color
-    //*(char *)pixel_ptr = pixel_color;
-    VGA_PIXEL(col, row, pixel_color);
-  }
-  for (xc = -r; xc <= r; xc++)
-  {
-    // row = yc;
-    row1 = (int)sqrt((float)(rsqr + r - xc * xc));
-    // right edge
-    col = xc + x;   // add the center point
-    row = row1 + y; // add the center point
-    // check for valid 640x480
-    if (col > 639)
-      col = 639;
-    if (row > 479)
-      row = 479;
-    if (col < 0)
-      col = 0;
-    if (row < 0)
-      row = 0;
-    // pixel_ptr = (char *)vga_pixel_ptr + (row<<10) + col ;
-    //  set pixel color
-    //*(char *)pixel_ptr = pixel_color;
-    VGA_PIXEL(col, row, pixel_color);
-    // left edge
-    row = -row1 + y; // add the center point
-    // check for valid 640x480
-    if (col > 639)
-      col = 639;
-    if (row > 479)
-      row = 479;
-    if (col < 0)
-      col = 0;
-    if (row < 0)
-      row = 0;
-    // pixel_ptr = (char *)vga_pixel_ptr + (row<<10) + col ;
-    //  set pixel color
-    //*(char *)pixel_ptr = pixel_color;
     VGA_PIXEL(col, row, pixel_color);
   }
 }
@@ -938,10 +651,6 @@ void VGA_line(int x1, int y1, int x2, int y2, short c)
 
   for (j = 0; j <= dx; j++)
   {
-    // video_pt(x,y,c); //640x480
-    // pixel_ptr = (char *)vga_pixel_ptr + (y<<10)+ x;
-    //  set pixel color
-    //*(char *)pixel_ptr = c;
     VGA_PIXEL(x, y, c);
 
     if (e >= 0)
