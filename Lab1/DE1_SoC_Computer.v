e3e5d27999195787a02a5903eef95d152bbf8875
kw456 <46826017+kw456@users.noreply.github.com>
2022-02-17 13:33:04 -0500

    Added comments to Verilog

--- Lab1/DE1_SoC_Computer.v
+++ Lab1/DE1_SoC_Computer.v
@@ -1,278 +1,3 @@
-
-
-module DE1_SoC_Computer (
-	////////////////////////////////////
-	// FPGA Pins
-	////////////////////////////////////
-
-	// Clock pins
-	CLOCK_50,
-	CLOCK2_50,
-	CLOCK3_50,
-	CLOCK4_50,
-
-	// ADC
-	ADC_CS_N,
-	ADC_DIN,
-	ADC_DOUT,
-	ADC_SCLK,
-
-	// Audio
-	AUD_ADCDAT,
-	AUD_ADCLRCK,
-	AUD_BCLK,
-	AUD_DACDAT,
-	AUD_DACLRCK,
-	AUD_XCK,
-
-	// SDRAM
-	DRAM_ADDR,
-	DRAM_BA,
-	DRAM_CAS_N,
-	DRAM_CKE,
-	DRAM_CLK,
-	DRAM_CS_N,
-	DRAM_DQ,
-	DRAM_LDQM,
-	DRAM_RAS_N,
-	DRAM_UDQM,
-	DRAM_WE_N,
-
-	// I2C Bus for Configuration of the Audio and Video-In Chips
-	FPGA_I2C_SCLK,
-	FPGA_I2C_SDAT,
-
-	// 40-Pin Headers
-	GPIO_0,
-	GPIO_1,
-	
-	// Seven Segment Displays
-	HEX0,
-	HEX1,
-	HEX2,
-	HEX3,
-	HEX4,
-	HEX5,
-
-	// IR
-	IRDA_RXD,
-	IRDA_TXD,
-
-	// Pushbuttons
-	KEY,
-
-	// LEDs
-	LEDR,
-
-	// PS2 Ports
-	PS2_CLK,
-	PS2_DAT,
-	
-	PS2_CLK2,
-	PS2_DAT2,
-
-	// Slider Switches
-	SW,
-
-	// Video-In
-	TD_CLK27,
-	TD_DATA,
-	TD_HS,
-	TD_RESET_N,
-	TD_VS,
-
-	// VGA
-	VGA_B,
-	VGA_BLANK_N,
-	VGA_CLK,
-	VGA_G,
-	VGA_HS,
-	VGA_R,
-	VGA_SYNC_N,
-	VGA_VS,
-
-	////////////////////////////////////
-	// HPS Pins
-	////////////////////////////////////
-	
-	// DDR3 SDRAM
-	HPS_DDR3_ADDR,
-	HPS_DDR3_BA,
-	HPS_DDR3_CAS_N,
-	HPS_DDR3_CKE,
-	HPS_DDR3_CK_N,
-	HPS_DDR3_CK_P,
-	HPS_DDR3_CS_N,
-	HPS_DDR3_DM,
-	HPS_DDR3_DQ,
-	HPS_DDR3_DQS_N,
-	HPS_DDR3_DQS_P,
-	HPS_DDR3_ODT,
-	HPS_DDR3_RAS_N,
-	HPS_DDR3_RESET_N,
-	HPS_DDR3_RZQ,
-	HPS_DDR3_WE_N,
-
-	// Ethernet
-	HPS_ENET_GTX_CLK,
-	HPS_ENET_INT_N,
-	HPS_ENET_MDC,
-	HPS_ENET_MDIO,
-	HPS_ENET_RX_CLK,
-	HPS_ENET_RX_DATA,
-	HPS_ENET_RX_DV,
-	HPS_ENET_TX_DATA,
-	HPS_ENET_TX_EN,
-
-	// Flash
-	HPS_FLASH_DATA,
-	HPS_FLASH_DCLK,
-	HPS_FLASH_NCSO,
-
-	// Accelerometer
-	HPS_GSENSOR_INT,
-		
-	// General Purpose I/O
-	HPS_GPIO,
-		
-	// I2C
-	HPS_I2C_CONTROL,
-	HPS_I2C1_SCLK,
-	HPS_I2C1_SDAT,
-	HPS_I2C2_SCLK,
-	HPS_I2C2_SDAT,
-
-	// Pushbutton
-	HPS_KEY,
-
-	// LED
-	HPS_LED,
-		
-	// SD Card
-	HPS_SD_CLK,
-	HPS_SD_CMD,
-	HPS_SD_DATA,
-
-	// SPI
-	HPS_SPIM_CLK,
-	HPS_SPIM_MISO,
-	HPS_SPIM_MOSI,
-	HPS_SPIM_SS,
-
-	// UART
-	HPS_UART_RX,
-	HPS_UART_TX,
-
-	// USB
-	HPS_CONV_USB_N,
-	HPS_USB_CLKOUT,
-	HPS_USB_DATA,
-	HPS_USB_DIR,
-	HPS_USB_NXT,
-	HPS_USB_STP
-);
-
-//=======================================================
-//  PARAMETER declarations
-//=======================================================
-
-
-//=======================================================
-//  PORT declarations
-//=======================================================
-
-////////////////////////////////////
-// FPGA Pins
-////////////////////////////////////
-
-// Clock pins
-input						CLOCK_50;
-input						CLOCK2_50;
-input						CLOCK3_50;
-input						CLOCK4_50;
-
-// ADC
-inout						ADC_CS_N;
-output					ADC_DIN;
-input						ADC_DOUT;
-output					ADC_SCLK;
-
-// Audio
-input						AUD_ADCDAT;
-inout						AUD_ADCLRCK;
-inout						AUD_BCLK;
-output					AUD_DACDAT;
-inout						AUD_DACLRCK;
-output					AUD_XCK;
-
-// SDRAM
-output 		[12: 0]	DRAM_ADDR;
-output		[ 1: 0]	DRAM_BA;
-output					DRAM_CAS_N;
-output					DRAM_CKE;
-output					DRAM_CLK;
-output					DRAM_CS_N;
-inout			[15: 0]	DRAM_DQ;
-output					DRAM_LDQM;
-output					DRAM_RAS_N;
-output					DRAM_UDQM;
-output					DRAM_WE_N;
-
-// I2C Bus for Configuration of the Audio and Video-In Chips
-output					FPGA_I2C_SCLK;
-inout						FPGA_I2C_SDAT;
-
-// 40-pin headers
-inout			[35: 0]	GPIO_0;
-inout			[35: 0]	GPIO_1;
-
-// Seven Segment Displays
-output		[ 6: 0]	HEX0;
-output		[ 6: 0]	HEX1;
-output		[ 6: 0]	HEX2;
-output		[ 6: 0]	HEX3;
-output		[ 6: 0]	HEX4;
-output		[ 6: 0]	HEX5;
-
-// IR
-input						IRDA_RXD;
-output					IRDA_TXD;
-
-// Pushbuttons
-input			[ 3: 0]	KEY;
-
-// LEDs
-output		[ 9: 0]	LEDR;
-
-// PS2 Ports
-inout						PS2_CLK;
-inout						PS2_DAT;
-
-inout						PS2_CLK2;
-inout						PS2_DAT2;
-
-// Slider Switches
-input			[ 9: 0]	SW;
-
-// Video-In
-input						TD_CLK27;
-input			[ 7: 0]	TD_DATA;
-input						TD_HS;
-output					TD_RESET_N;
-input						TD_VS;
-
-// VGA
-output		[ 7: 0]	VGA_B;
-output					VGA_BLANK_N;
-output					VGA_CLK;
-output		[ 7: 0]	VGA_G;
-output					VGA_HS;
-output		[ 7: 0]	VGA_R;
-output					VGA_SYNC_N;
-output					VGA_VS;
-
-
-
 ////////////////////////////////////
 // HPS Pins
 ////////////////////////////////////
@@ -294,7 +19,6 @@ output					HPS_DDR3_RAS_N;
 output					HPS_DDR3_RESET_N;
 input						HPS_DDR3_RZQ;
 output					HPS_DDR3_WE_N;
-
 // Ethernet
 output					HPS_ENET_GTX_CLK;
 inout						HPS_ENET_INT_N;
@@ -305,46 +29,36 @@ input			[ 3: 0]	HPS_ENET_RX_DATA;
 input						HPS_ENET_RX_DV;
 output		[ 3: 0]	HPS_ENET_TX_DATA;
 output					HPS_ENET_TX_EN;
-
 // Flash
 inout			[ 3: 0]	HPS_FLASH_DATA;
 output					HPS_FLASH_DCLK;
 output					HPS_FLASH_NCSO;
-
 // Accelerometer
 inout						HPS_GSENSOR_INT;
-
 // General Purpose I/O
 inout			[ 1: 0]	HPS_GPIO;
-
 // I2C
 inout						HPS_I2C_CONTROL;
 inout						HPS_I2C1_SCLK;
 inout						HPS_I2C1_SDAT;
 inout						HPS_I2C2_SCLK;
 inout						HPS_I2C2_SDAT;
-
 // Pushbutton
 inout						HPS_KEY;
-
 // LED
 inout						HPS_LED;
-
 // SD Card
 output					HPS_SD_CLK;
 inout						HPS_SD_CMD;
 inout			[ 3: 0]	HPS_SD_DATA;
-
 // SPI
 output					HPS_SPIM_CLK;
 input						HPS_SPIM_MISO;
 output					HPS_SPIM_MOSI;
 inout						HPS_SPIM_SS;
-
 // UART
 input						HPS_UART_RX;
 output					HPS_UART_TX;
-
 // USB
 inout						HPS_CONV_USB_N;
 input						HPS_USB_CLKOUT;
@@ -354,40 +68,12 @@ input						HPS_USB_NXT;
 output					HPS_USB_STP;
 
 //=======================================================
-//  REG/WIRE declarations
-//=======================================================
-
-wire			[15: 0]	hex3_hex0;
-//wire			[15: 0]	hex5_hex4;
-
-//assign HEX0 = ~hex3_hex0[ 6: 0]; // hex3_hex0[ 6: 0]; 
-//assign HEX1 = ~hex3_hex0[14: 8];
-//assign HEX2 = ~hex3_hex0[22:16];
-//assign HEX3 = ~hex3_hex0[30:24];
-assign HEX4 = 7'b1111111;
-assign HEX5 = 7'b1111111;
-
-HexDigit Digit0(HEX0, hex3_hex0[3:0]);
-HexDigit Digit1(HEX1, hex3_hex0[7:4]);
-HexDigit Digit2(HEX2, hex3_hex0[11:8]);
-HexDigit Digit3(HEX3, hex3_hex0[15:12]);
-
-
-//=======================================================
 // PIO state machine
 //=======================================================
 // OUTPUTs from the FPGA, INPUT to HPS
 wire [31:0] pp_in_lw_axi ;
 // INPUTS to the FPGA, OUTPUT from HPS
 wire [31:0] pp_out_lw_axi ;
-
-// for debugging:
-// SW[0] chooses to look at AXI input or output on HEX display
-// SW[1] chooses input+1 or zero to go back to HPS on AXI bus
-// SW[2] chooses input+1 or zero to go back to HPS on light-weight AXI
-// assign pp_in_axi = (SW[1])? (pp_out_axi + 32'd1) : 0 ;
-// assign pp_in_lw_axi = (SW[2])? (pp_out_lw_axi + 32'd1) : 0 ;
-// assign hex3_hex0 = (SW[0])? pp_in_axi : pp_out_axi ;
 
 //=======================================================
 // Functioning Kernel
@@ -421,30 +107,25 @@ Computer_System The_System (
 	////////////////////////////////////
 	// FPGA Side
 	////////////////////////////////////
-
 	// Global signals
 	.system_pll_ref_clk_clk					(CLOCK_50),
 	.system_pll_ref_reset_reset			(1'b0),
-	
-	// PIO ports
+	// PIO ports 
 	.pio_x_lw_input_external_connection_export (x_out),
 	.pio_y_lw_input_external_connection_export (y_out),
 	.pio_z_lw_input_external_connection_export (z_out),     
 	.init_x_output_external_connection_export  (init_x),
-	.init_y_output_external_connection_export	 (init_y),
+	.init_y_output_external_connection_export  (init_y),
 	.init_z_output_external_connection_export  (init_z),
 	.sigma_output_external_connection_export   (sigma),
 	.beta_output_external_connection_export    (beta),
 	.rho_output_external_connection_export     (rho), 
 	.dt_output_external_connection_export      (dt), 
-	
 	.pp_in_lw_axi_export                       (pp_in_lw_axi),                      
 	.pp_out_lw_axi_export                      (pp_out_lw_axi),                     
-
 	// AV Config
 	.av_config_SCLK							(FPGA_I2C_SCLK),
 	.av_config_SDAT							(FPGA_I2C_SDAT),
-
 	// VGA Subsystem
 	.vga_pll_ref_clk_clk 					(CLOCK2_50),
 	.vga_pll_ref_reset_reset				(1'b0),
@@ -456,7 +137,6 @@ Computer_System The_System (
 	.vga_R										(VGA_R),
 	.vga_G										(VGA_G),
 	.vga_B										(VGA_B),
-	
 	// SDRAM
 	.sdram_clk_clk								(DRAM_CLK),
    .sdram_addr									(DRAM_ADDR),
@@ -488,8 +168,7 @@ Computer_System The_System (
 	.memory_mem_dqs_n		(HPS_DDR3_DQS_N),
 	.memory_mem_odt		(HPS_DDR3_ODT),
 	.memory_mem_dm			(HPS_DDR3_DM),
-	.memory_oct_rzqin		(HPS_DDR3_RZQ),
-		  
+	.memory_oct_rzqin		(HPS_DDR3_RZQ),	  
 	// Ethernet
 	.hps_io_hps_io_gpio_inst_GPIO35	(HPS_ENET_INT_N),
 	.hps_io_hps_io_emac1_inst_TX_CLK	(HPS_ENET_GTX_CLK),
@@ -506,7 +185,6 @@ Computer_System The_System (
 	.hps_io_hps_io_emac1_inst_RXD1	(HPS_ENET_RX_DATA[1]),
 	.hps_io_hps_io_emac1_inst_RXD2	(HPS_ENET_RX_DATA[2]),
 	.hps_io_hps_io_emac1_inst_RXD3	(HPS_ENET_RX_DATA[3]),
-
 	// Flash
 	.hps_io_hps_io_qspi_inst_IO0	(HPS_FLASH_DATA[0]),
 	.hps_io_hps_io_qspi_inst_IO1	(HPS_FLASH_DATA[1]),
@@ -514,32 +192,25 @@ Computer_System The_System (
 	.hps_io_hps_io_qspi_inst_IO3	(HPS_FLASH_DATA[3]),
 	.hps_io_hps_io_qspi_inst_SS0	(HPS_FLASH_NCSO),
 	.hps_io_hps_io_qspi_inst_CLK	(HPS_FLASH_DCLK),
-
 	// Accelerometer
 	.hps_io_hps_io_gpio_inst_GPIO61	(HPS_GSENSOR_INT),
-
 	//.adc_sclk                        (ADC_SCLK),
 	//.adc_cs_n                        (ADC_CS_N),
 	//.adc_dout                        (ADC_DOUT),
 	//.adc_din                         (ADC_DIN),
-
 	// General Purpose I/O
 	.hps_io_hps_io_gpio_inst_GPIO40	(HPS_GPIO[0]),
 	.hps_io_hps_io_gpio_inst_GPIO41	(HPS_GPIO[1]),
-
 	// I2C
 	.hps_io_hps_io_gpio_inst_GPIO48	(HPS_I2C_CONTROL),
 	.hps_io_hps_io_i2c0_inst_SDA		(HPS_I2C1_SDAT),
 	.hps_io_hps_io_i2c0_inst_SCL		(HPS_I2C1_SCLK),
 	.hps_io_hps_io_i2c1_inst_SDA		(HPS_I2C2_SDAT),
 	.hps_io_hps_io_i2c1_inst_SCL		(HPS_I2C2_SCLK),
-
 	// Pushbutton
 	.hps_io_hps_io_gpio_inst_GPIO54	(HPS_KEY),
-
 	// LED
 	.hps_io_hps_io_gpio_inst_GPIO53	(HPS_LED),
-
 	// SD Card
 	.hps_io_hps_io_sdio_inst_CMD	(HPS_SD_CMD),
 	.hps_io_hps_io_sdio_inst_D0	(HPS_SD_DATA[0]),
@@ -547,17 +218,14 @@ Computer_System The_System (
 	.hps_io_hps_io_sdio_inst_CLK	(HPS_SD_CLK),
 	.hps_io_hps_io_sdio_inst_D2	(HPS_SD_DATA[2]),
 	.hps_io_hps_io_sdio_inst_D3	(HPS_SD_DATA[3]),
-
 	// SPI
 	.hps_io_hps_io_spim1_inst_CLK		(HPS_SPIM_CLK),
 	.hps_io_hps_io_spim1_inst_MOSI	(HPS_SPIM_MOSI),
 	.hps_io_hps_io_spim1_inst_MISO	(HPS_SPIM_MISO),
 	.hps_io_hps_io_spim1_inst_SS0		(HPS_SPIM_SS),
-
 	// UART
 	.hps_io_hps_io_uart0_inst_RX	(HPS_UART_RX),
 	.hps_io_hps_io_uart0_inst_TX	(HPS_UART_TX),
-
 	// USB
 	.hps_io_hps_io_gpio_inst_GPIO09	(HPS_CONV_USB_N),
 	.hps_io_hps_io_usb1_inst_D0		(HPS_USB_DATA[0]),
@@ -573,13 +241,13 @@ Computer_System The_System (
 	.hps_io_hps_io_usb1_inst_DIR		(HPS_USB_DIR),
 	.hps_io_hps_io_usb1_inst_NXT		(HPS_USB_NXT)
 );
-
-
 endmodule
 
 //============================================================
 // ODE Solver
 //============================================================
+// Inputs: clk, reset, dt, init x/y/z, beta, sigma, rho
+// Outputs: x, y, z
 module ODEsolver (
   input clk,
   input reset,
@@ -603,19 +271,25 @@ module ODEsolver (
   assign y = y_reg;
   assign z = z_reg;
 
+
   always @ (posedge clk) begin
+    // If reset, initialize variables
     if (reset) begin
       x_reg <= init_x;
       y_reg <= init_y;
       z_reg <= init_z;
+    // Else, add calculated accumulation (integrate)
     end else begin
+      // x <= dx*dt + x;
       x_reg <= x_calc+x;
+      // y <= dy*dt + y;
       y_reg <= y_calc+y;
+      // z <= dz*dt + z;
       z_reg <= z_calc+z;
     end
   end
 
-  // x_calc = sigma*(y-x)*dt
+  // x_calc = dx*dt = sigma*(y-x)*dt 
   signed_mult x_mult_1 (
     .a(y-x),
     .b(dt),
@@ -627,7 +301,7 @@ module ODEsolver (
     .out(x_calc)
   );
 
-  // y_calc = (x*(rho-z)-y)*dt
+  // y_calc = dy*dt = (x*(rho-z)-y)*dt
   signed_mult y_mult_1 (
     .a(x),
     .b(dt),
@@ -645,7 +319,7 @@ module ODEsolver (
   );
   assign y_calc = y_inter2 - y_inter3;
 
-  // z_calc = (x*y-beta*z)*dt
+  // z_calc = dz*dt = (x*y-beta*z)*dt
   signed_mult z_mult_1 (
     .a(x),
     .b(dt),
@@ -668,7 +342,6 @@ module ODEsolver (
   );
   assign z_calc = x_mult_y - beta_mult_z;
 endmodule
-
 
 //============================================================
 // Fixed-Point Multiplier
