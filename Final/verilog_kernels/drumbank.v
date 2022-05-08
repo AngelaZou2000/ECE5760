3160959629963ffc399ccbf237363bcd86d44eb3
AngelaZou2000 <az292@cornell.edu>
2022-05-08 11:30:32 -0400

    small fix: add the write stages to store first input mapping to M10K

--- Final/verilog_kernels/drumbank.v
+++ Final/verilog_kernels/drumbank.v
@@ -22,8 +22,6 @@ module drumbank #(parameter BANK_SIZE=1) (
   output wire bank_fault
 );
 
-  // TODO: initialize M10K value
-
   wire [BANK_SIZE-1:0] done, fault;
   wire [25:0] plugboard_setting [BANK_SIZE:0];
   wire [4:0] plugboard_pass_mapping [BANK_SIZE:0];
@@ -31,17 +29,60 @@ module drumbank #(parameter BANK_SIZE=1) (
   wire [4:0] plugboard_write_address [BANK_SIZE-1:0];
   wire [4:0] plugboard_read_address [BANK_SIZE-1:0];
   wire [4:0] plugboard_write_msg [BANK_SIZE-1:0];
-  wire [4:0] plugboard_read_msg [BANK_SIZE-1:0];
+  // wire [4:0] plugboard_read_msg [BANK_SIZE-1:0];
+  reg INIT_plugboard_write_enable;
+  reg [4:0] INIT_plugboard_write_address;
+  reg [4:0] INIT_plugboard_read_address;
+  reg [4:0] INIT_plugboard_write_msg;
   wire valid_plugboard_write_enable;
   wire [4:0] valid_plugboard_write_address;
   wire [4:0] valid_plugboard_read_address;
   wire [4:0] valid_plugboard_write_msg;
   wire [4:0] valid_plugboard_read_msg;
   reg [4:0] drum_stage_count;
+  reg drum0_enable;
   assign plugboard_setting[0] = plugboard_in;
   assign plugboard_pass_mapping[0] = plugboard_passin_mapping;
   assign bank_done = &done;
   assign bank_fault = |fault;
+
+  //TODO: write the drum0 input mapping to the M10k
+  localparam INIT = 2'b0;
+  localparam WRITE0 = 2'b1;
+  localparam WRITE1 = 2'd2;
+  localparam RUN = 2'd3;
+  reg [1:0] state_reg, state_next;
+
+  always@(*) begin
+    if (state_reg==INIT) state_next = WRITE0;
+    else if (state_reg==WRITE0) state_next = WRITE1;
+    else if (state_reg==WRITE1) state_next = RUN;
+    else state_next = state_reg;
+  end
+  always@(posedge clk) begin
+    if (reset) state_reg <= INIT;
+    else state_reg <= state_next;
+  end
+  always@(*) begin
+    if (state_reg==INIT) begin
+      drum0_enable = 1'b0;
+    end
+    else if (state_reg==WRITE0) begin
+      drum0_enable = 1'b0;
+      INIT_plugboard_write_enable = 1'b1;
+      INIT_plugboard_write_address = msg_input;
+      INIT_plugboard_write_msg = plugboard_passin_mapping;
+    end
+    else if (state_reg==WRITE1) begin
+      drum0_enable = 1'b0;
+      INIT_plugboard_write_enable = 1'b1;
+      INIT_plugboard_write_address = plugboard_passin_mapping;
+      INIT_plugboard_write_msg = msg_input;
+    end
+    else if (state_reg==RUN) begin
+      drum0_enable = 1'b1;
+    end
+  end
 
   genvar inst;
   generate
@@ -49,7 +90,7 @@ module drumbank #(parameter BANK_SIZE=1) (
       drum drum_inst (
         .clk                        (clk),
         .reset                      (reset),
-        .enable                     (inst==0?1'b1:done[inst-1]),
+        .enable                     (inst==0?drum0_enable:done[inst-1]),
         .done                       (done[inst]),
         .fault                      (fault[inst]),
         .msg_input                  (msg_input[5*inst+4:5*inst]),
@@ -83,10 +124,10 @@ module drumbank #(parameter BANK_SIZE=1) (
     drum_stage_count <= done[0]+done[1]+done[2]+done[3]+done[4]+done[5]+done[6]+done[7]+done[8]+done[9]+done[10]+done[11];
   end
 
-  assign valid_plugboard_write_enable = plugboard_write_enable[drum_stage_count];
-  assign valid_plugboard_write_address = plugboard_write_address[drum_stage_count];
-  assign valid_plugboard_write_msg = plugboard_write_msg[drum_stage_count];
-  assign valid_plugboard_read_address = plugboard_read_address[drum_stage_count];
+  assign valid_plugboard_write_enable = (state_reg!=RUN)?INIT_plugboard_write_enable:plugboard_write_enable[drum_stage_count];
+  assign valid_plugboard_write_address = (state_reg!=RUN)?INIT_plugboard_write_address:plugboard_write_address[drum_stage_count];
+  assign valid_plugboard_write_msg = (state_reg!=RUN)?INIT_plugboard_write_msg:plugboard_write_msg[drum_stage_count];
+  assign valid_plugboard_read_address = (state_reg!=RUN)?INIT_plugboard_read_address:plugboard_read_address[drum_stage_count];
 
   M10K #(5, 26) plugboard (
     .clk            (clk),
